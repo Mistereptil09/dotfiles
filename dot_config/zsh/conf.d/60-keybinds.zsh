b780f1521af7cde1ef453491f18338aaa1af7928
Ciorba Antonio <144670129+Mistereptil09@users.noreply.github.com>
2026-02-24 14:02:50 +0100

    Delete dot_config/zsh/conf.d/60-keybinds.zsh

--- dot_config/zsh/conf.d/60-keybinds.zsh
+++
@@ -1,5 +0,0 @@
-# ─── Key Bindings ─────────────────────────────────────────────────────────────
-if (( $+functions[history-substring-search-up] )); then
-  bindkey '^[[A' history-substring-search-up
-  bindkey '^[[B' history-substring-search-down
-fi
